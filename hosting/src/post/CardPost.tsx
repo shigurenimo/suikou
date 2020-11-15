661f74fe6bde9a261dc565431adfda56230c42c7
reiwa <reiwa@outlook.com.vn>
2020-11-15 22:25:42 +0700

    fix: 日付を修正する

--- hosting/src/post/CardPost.tsx
+++ hosting/src/post/CardPost.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from 'react'
 import ReactMarkdown from 'react-markdown'
+import { toDateText } from '../text/toDateText'
 import { NewsPost } from '../types/newsPost'
 import { AnchorURL } from './AnchorURL'
 import { DivisionImage } from './DivisionImage'
@@ -20,9 +21,7 @@ export const CardPost: FunctionComponent<Props> = ({ post }) => {
     <div className={'rounded-lg p-4 border border-gray-400 bg-gray-100'}>
       <h1 className={'font-bold text-lg'}>{post.title}</h1>
       <h2 className={'pt-1 text-sm'}>{post.title_en}</h2>
-      <p className={'text-sm pt-2'}>
-        {post.date.replace('/', '月').replace('/', '日')}
-      </p>
+      <p className={'text-sm pt-2'}>{toDateText(post.date)}</p>
       {hasActions && (
         <div className={'pt-2'}>
           {hasPDF && <AnchorURL href={post.image}>{'PDFファイル'}</AnchorURL>}
---
+++ hosting/src/text/toDateText.ts
@@ -0,0 +1,3 @@
+export const toDateText = (text: string) => {
+  return text.replace('/', '年').replace('/', '月') + '日'
+}
