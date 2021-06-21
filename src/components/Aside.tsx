6c1f59fce16034fedc93f0cfdf1cd5ef470b720a
haruta24 <mptjapw@gmail.com>
2021-06-21 15:52:10 +0900

    update

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,5 +1,6 @@
+import { Stack } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
-import { FunctionComponent } from 'react'
+import React, { FunctionComponent } from 'react'
 import { Footer } from './Footer'
 import { LinkAnchor } from './LinkAnchor'
 
@@ -7,74 +8,77 @@ export const Aside: FunctionComponent = () => {
   const { pathname } = useRouter()
 
   return (
-    <aside className={'w-full max-w-xs hidden md:block'}>
-      <div className={'fixed w-full max-w-xs'}>
-        <ul>
-          <li>
-            <LinkAnchor active={pathname === '/'} href={'/'}>
-              {'ホーム'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor active={pathname === '/nakaza'} href={'/nakaza'}>
-              {'仲座 栄三'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor active={pathname === '/about'} href={'/about'}>
-              {'水圏工学研究室'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor active={pathname === '/access'} href={'/access'}>
-              {'アクセス'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor active={pathname.startsWith('/news')} href={'/news'}>
-              {'お知らせ'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor
-              active={pathname.startsWith('/articles')}
-              href={'/articles'}
-            >
-              {'メディア掲載'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor
-              active={pathname.startsWith('/classes')}
-              href={'/classes'}
-            >
-              {'eラーニング'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor
-              active={pathname.startsWith('/feedbacks')}
-              href={'/feedbacks'}
-            >
-              {'授業の感想'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor
-              active={pathname.startsWith('/surveys')}
-              href={'/surveys'}
-            >
-              {'東北地方大津波災害調査'}
-            </LinkAnchor>
-          </li>
-          <li className={'mt-4'}>
-            <LinkAnchor active={pathname.startsWith('/books')} href={'/books'}>
-              {'書籍'}
-            </LinkAnchor>
-          </li>
-        </ul>
+    <Stack
+      as={'aside'}
+      display={{ base: 'none', md: 'block' }}
+      w={'100%'}
+      maxW={'xs'}
+    >
+      <div>
+        <Stack pb={4} fontWeight={'bold'}>
+          <LinkAnchor active={pathname === '/'} href={'/'}>
+            {'ホーム'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor active={pathname === '/nakaza'} href={'/nakaza'}>
+            {'仲座 栄三'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor active={pathname === '/about'} href={'/about'}>
+            {'水圏工学研究室'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor active={pathname === '/access'} href={'/access'}>
+            {'アクセス'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor active={pathname.startsWith('/news')} href={'/news'}>
+            {'お知らせ'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor
+            active={pathname.startsWith('/articles')}
+            href={'/articles'}
+          >
+            {'メディア掲載'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor
+            active={pathname.startsWith('/classes')}
+            href={'/classes'}
+          >
+            {'eラーニング'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor
+            active={pathname.startsWith('/feedbacks')}
+            href={'/feedbacks'}
+          >
+            {'授業の感想'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor
+            active={pathname.startsWith('/surveys')}
+            href={'/surveys'}
+          >
+            {'東北地方大津波災害調査'}
+          </LinkAnchor>
+        </Stack>
+        <Stack paddingBottom={'4'} fontWeight={'bold'}>
+          <LinkAnchor active={pathname.startsWith('/books')} href={'/books'}>
+            {'書籍'}
+          </LinkAnchor>
+        </Stack>
         <Footer />
       </div>
-    </aside>
+    </Stack>
   )
 }
