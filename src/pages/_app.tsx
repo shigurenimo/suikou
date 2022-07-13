efd8833c656da8c9c673f884777fd8cf302e8f92
reiwa <reiwa@outlook.com.vn>
2022-07-14 07:36:56 +0900

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,35 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/app"
-import { useRouter } from "next/router"
-import React, { FC } from "react"
+import React, { FC, useEffect } from "react"
 import { Aside } from "src/components/Aside"
 import "src/index.css"
 import { theme } from "src/theme"
+import { getApps, initializeApp } from "firebase/app"
+import { Router } from "next/router"
+import {
+  logEvent,
+  getAnalytics,
+  setAnalyticsCollectionEnabled,
+} from "firebase/analytics"
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  const router = useRouter()
-
-  // useEffect(() => {
-  //   const handleRouteChange = (path: string) => {
-  //     gtag("config", "G-6ZRNQNDK1C", {
-  //       page_path: path,
-  //     })
-  //   }
-  //   router.events.on("routeChangeComplete", handleRouteChange)
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange)
-  //   }
-  // }, [])
+  useEffect(() => {
+    if (typeof window === "undefined") return
+    if (process.env.NODE_ENV !== "production") return
+    const routeChangeComplete = () => {
+      if (getApps().length === 0) return
+      logEvent(getAnalytics(), "page_view", {
+        page_location: document.title,
+        page_path: location.href,
+        page_title: location.pathname,
+      })
+    }
+    Router.events.on("routeChangeComplete", routeChangeComplete)
+    routeChangeComplete()
+    return () => {
+      Router.events.off("routeChangeComplete", routeChangeComplete)
+    }
+  }, [])
 
   return (
     <ChakraProvider theme={theme}>
@@ -27,6 +37,21 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
       <Component {...pageProps} />
     </ChakraProvider>
   )
+}
+
+if (getApps().length === 0) {
+  initializeApp({
+    apiKey: "AIzaSyApe1mnUDKLGUIXa0XtntxOe-k2vysY8L4",
+    authDomain: "ejik3yfgcuhta376sw2p.firebaseapp.com",
+    projectId: "ejik3yfgcuhta376sw2p",
+    storageBucket: "ejik3yfgcuhta376sw2p.appspot.com",
+    messagingSenderId: "1056518212822",
+    appId: "1:1056518212822:web:ca40589b75cbf38a05cd77",
+    measurementId: "G-Q1B4GLE32V",
+  })
+  if (process.env.NODE_ENV === "production") {
+    setAnalyticsCollectionEnabled(getAnalytics(), true)
+  }
 }
 
 export default App
